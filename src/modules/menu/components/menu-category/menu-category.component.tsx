d131e09da6cdfa6a96a71ec35b0476d9b5f84985
Serhii <s.saviuk@apiko.com>
2022-12-25 17:47:53 +0200

    add slug id to menu category

--- src/modules/menu/components/menu-category/menu-category.component.tsx
+++ src/modules/menu/components/menu-category/menu-category.component.tsx
@@ -8,7 +8,7 @@ interface MenuCategoryProps {
 
 export const MenuCategory: FC<MenuCategoryProps> = ({ data }) => {
 	return (
-		<div className="w-full flex flex-col gap-12">
+		<div className="w-full flex flex-col gap-12" id={data.slug}>
 			<div className="flex flex-col gap-1 items-center">
 				<h2 className="font-bold text-2xl relative hover:after:content-[''] after:block after:absolute after:w-full after:h-1 after:bg-amber-400">
 					{data.title}
